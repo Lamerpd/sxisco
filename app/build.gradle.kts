fc23ab56a432d2dfc76e38f892796e47533b7d09
lamerpd <Eder.rodelas@hotmail.com>
2026-08-07 07:09:51 -0300

    bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.sxisco.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 4
+        versionName = "1.0.2"
     }
 
     buildTypes {
